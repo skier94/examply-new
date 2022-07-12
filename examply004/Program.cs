87422f84d81d0d25209e700ce8e2f9ad1f9a96f1
Vitaly <vitalij-kurbatov@mail.ru>
2022-07-12 10:16:15 +0300

    new 004

--- examply004/Program.cs
+++ examply004/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA / numberB);
+
